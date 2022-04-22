53eadd0191dc5cc18bf3acbb4459f8a8681311c2
ShengSir1 <1823664370@qq.com>
2022-04-22 17:21:37 +0800

    three

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <template>
   <!-- Vue3组件中的模板结构可以没有根标签 -->
   <h1>一个人的信息</h1>
-  <h2>姓名：{{person.name}}</h2>
+  <h2 v-show="person.name">姓名：{{person.name}}</h2>
   <h2>年龄：{{person.age}}</h2>
+  <h2 v-show="person.sex">性别：{{person.sex}}</h2>
   <h2>爱好：{{person.hobby}}</h2>
   <h3>岗位：{{person.job.type}}</h3>
   <h3>薪资：{{person.job.salary}}</h3>
   <button @click="changeInfo">修改这个人的信息</button>
+  <button @click="addSex">添加一个sex属性</button>
+  <button @click="deleteName">删除一个name属性</button>
 </template>
 
 <script>
@@ -37,11 +40,18 @@
       person.hobby[2] = '社交'
       // console.log(name,age);
     }
-
+    function addSex(){
+      person.sex='男'
+    }
+    function deleteName(){
+      delete person.name
+    }
     // 返回一个对象(常用)
     return {
       person,
       changeInfo,
+      addSex,
+      deleteName
     }
 
 
